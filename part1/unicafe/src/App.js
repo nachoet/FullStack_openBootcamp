4a5f64ed9d815af0be66d0180842d084cf0a6fd0
Nacho <n.etche@yandex.com>
2022-05-08 18:11:52 +0200

    unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,27 +5,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  // save all clicks to otherstate
+  const [allClicks, setAllClicks] = useState([])
 
   const handleGood = () => {
     const newGood = good + 1
     setGood(newGood)
+    setAllClicks(allClicks.concat('g'))
     // console.log(newGood)
   }
 
   const handleNeutral = () => {
     const newNeutral = neutral + 1
     setNeutral(newNeutral)
+    setAllClicks(allClicks.concat('n'))
     // console.log(newNeutral)
   }
 
   const handleBad = () => {
     const newBad = bad + 1
     setBad(newBad)
+    setAllClicks(allClicks.concat('b'))
   }
 
-  // const average = () => {
-  //   return (good + neutral + bad) / 3
-  // }
+  const Statistics = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <p>No feedback given yet!</p>
+      )
+
+    } else {
+      return (
+        <div>
+          <p>Good {good}</p>
+          <p>Neutral {neutral}</p>
+          <p>Bad {bad}</p>
+          <p>Overall {good + neutral + bad}</p>
+          <p>Averages: {(good + neutral + bad) / 3}</p>
+        </div>
+      )
+    }
+  }
 
   return (
     <div>
@@ -35,11 +55,8 @@ const App = () => {
       <button onClick={handleBad}>Bad</button>
 
       <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Overall {good + neutral + bad}</p>
-      <p>Average {(good + neutral + bad) / 3}</p>
+
+      <Statistics allClicks={allClicks}></Statistics>
     </div>
   )
 }
